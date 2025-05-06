832f3b24a008b7620a90f4deaa510f003aadd70f
Talita Cristina Alves Lobato <05058225204@>
2025-05-06 11:30:38 -0300

    em andamento 06-05

--- src/Componentes/Cabecalho.jsx
+++ src/Componentes/Cabecalho.jsx
@@ -2,7 +2,7 @@ import estilo from './Cabecalho.module.css';
 export function Cabecalho(){
     return(
         <header className={estilo.conteiner}>
-            <h1>Escola da DS15</h1>
+            <h1>Gerenciamento Escola SENAI</h1>
         </header>
     )
 }
--- src/Componentes/Menu.jsx
+++ src/Componentes/Menu.jsx
@@ -5,19 +5,27 @@ export function Menu(){
             <table>
                 <tr>
                     <td>
+                    <span className={`${estilo.icon} material-symbols-outlined`}>person</span>                        
+                        <br></br>
                         Professores
                     </td>
 
                     <td>
+                    <span className={`${estilo.icon} material-symbols-outlined`}>groups</span>
+                        <br></br>
                         Gestores
                     </td>
                 </tr>
                 <tr>
                     <td>
+                    <span className={`${estilo.icon} material-symbols-outlined`}>menu_book</span>
+                        <br></br>
                         Disciplinas
                     </td>
 
                     <td>
+                    <span className={`${estilo.icon} material-symbols-outlined`}>nature_people</span>
+                    <br></br>
                         Ambiente
                     </td>
                 </tr>
